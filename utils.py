edb9c021d0773a924a709bff6420e654e9f4fe6a
SimonPreissner <simon.preissner@gmx.de>
2019-12-19 13:41:10 +0100

    utils.Timer() is under construction.

--- utils.py
+++ utils.py
@@ -7,8 +7,8 @@ This module provides helper functions.
 
 import sys
 import re
-#from tqdm import tqdm
-#import time
+from tqdm import tqdm
+import time
 
 
 def loop_input(rtype=str, default=None, msg=""):
@@ -206,3 +206,92 @@ class ConfigReader():
 
     def set(self, paramname, value):
         self.params.update({paramname:value})
+
+
+
+
+class Timer():
+	#TODO change data structure to contain the type of time-taking; e.g., (t1, t0, lap)
+	#TODO change data structure to containe fragmented timing (multiple intervals)
+	#TODO provide better documentation and usage instructions
+	#TODO implement go_on()
+	#TODO change total() to perform simple cumulative counting (no END_TIME)
+    def __init__(self):
+        self.START_TIME = time.time() 
+        self.END_TIME = 0
+        self.t0 = 0
+        self.t_lap = 0
+        self.counter = 0
+        self.stopped_times = {}
+
+    def __repr__(self):
+        return "\n".join([str(round(t,6))+"   "+str(i) 
+                          for i,t in self.stopped_times])
+
+    def start(self):
+        """ resets t0 (= reference time) to now """
+        self.t0 = time.time()
+        self.t_lap = self.t0
+        return self.t0
+
+    def stop(self, label=None):
+        """ 
+        Takes time since t0, without resetting t0. 
+        Use this for cumulative timing. 
+        """
+        t1 = time.time()
+        if not label: label = self.counter
+        self.stopped_times[label] = (t1, self.t0)
+        self.counter += 1
+        return t1
+
+    def go_on(self, label=None):
+    	""" 
+        
+        """
+        t1 = time.time()
+        if not label: label = self.counter
+        self.stopped_times[label] = (t1, self.t0)
+        self.counter += 1
+        return t1
+
+    def lap(self, label=None):
+        """ 
+        Takes time and resets t_lap to now. 
+        Use this for subsequent intervals. 
+        """
+        t1 = time.time()
+        if not label: label = self.counter
+        self.stopped_times[label] = (t1, self.t_lap)
+        self.counter += 1
+        self.t_lap = t1
+        return t1
+
+    def total(self, label='total', ret_interval=True):
+        """ 
+        Like stop(), but takes time since initialization. 
+        Use this to take total runtime.
+        """
+        self.END_TIME = time.time()
+        interval = self.END_TIME - self.START_TIME
+        self.stopped_times[label] = (self.END_TIME, self.START_TIME)
+        if ret_interval:
+            return interval
+        else:
+            return self.END_TIME
+
+    def show(self, *labels, precision=4):
+        """ returns particular times, identified by their labels """
+        ret = ""
+        for label in labels:
+            t1, t0 = self.stopped_times[label]
+            ret += (str(round(t1-t0, precision))+" "+str(label)+"\n")
+        return ret.rstrip() # to get rid of the last newline
+
+    def show_all(self, precision=4):
+        """ shows all stored times with associated labels (or cardinal numbers if not specified) """
+        ret = ""
+        for label, (t1, t0) in self.stopped_times.items():
+            ret += (str(round(t1-t0, precision))+" "+str(label)+"\n")
+        return ret.rstrip() # to get rid of the last newline
+
